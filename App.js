d92ce7ef5e046fc3cb1ffa7338760276c6333ad2
navin <navinkumarsingh2002@outlook.com>
2024-06-12 20:25:45 +0530

    commit1

---
+++ App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import Navbar from './Components/Navbar';
+import TextForm from './Components/TextForm';
+
+
+function App() {
+  return (
+  <>
+  
+<Navbar title="TextUtils"/>
+<div className="container">
+  <TextForm heading="Text Analyzer"/>
+  </div>
+
+</>
+  );
+}
+
+export default App;
---
+++ Components/TextForm.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function TextForm(props) {
+  return (
+    <div>
+      <h1>{props.heading}</h1>
+      <div className="mb-3">
+        
+        <textarea
+          className="form-control"
+          id="myBox"
+          rows="5"
+        ></textarea>
+      </div>
+      <button className="btn btn-primary">Convert</button>
+      </div>
+    
+  );
+}
